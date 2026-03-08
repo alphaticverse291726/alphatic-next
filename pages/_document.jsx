7c692152477da17c8963b5deb35af9cbe7a579fa
alphaticverse291726 <alphaticverse291726@gmail.com>
2026-03-08 19:16:50 +0000

    added _app and _document files

---
+++ pages/_document.jsx
@@ -0,0 +1,32 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Meta tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="Alphatic - Intelligent Healthcare Platform"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
